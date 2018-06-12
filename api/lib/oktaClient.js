942c763f134e51f8050ecb7685d3e9e2156d08be
Lee Brandt <leebrandt@gmail.com>
2018-06-12 11:49:54 -0500

    Removed Lee's personal orgUrl, clientID and api token

--- api/lib/oktaClient.js
+++ api/lib/oktaClient.js
@@ -1,8 +1,8 @@
 const okta = require('@okta/okta-sdk-nodejs');
 
 const client = new okta.Client({
-  orgUrl: 'https://dev-846291.oktapreview.com',
-  token: '009w8chhKHawNgrOae2SKq8naL8wPK5BMtc_EnwLNX'
+  orgUrl: '{youtOktaDomain}',
+  token: '{APIToken}'
 });
 
 module.exports = client;
--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,6 +1,6 @@
 export default {
-  url: 'https://dev-846291.oktapreview.com',
-  issuer: 'https://dev-846291.oktapreview.com/oauth2/default',
+  url: '{youOktaDomain}',
+  issuer: '{youOktaDomain}/oauth2/default',
   redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oafg0hk5rvIF3Sq00h7'
+  client_id: '{ClientID}'
 }
